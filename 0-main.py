a8e17597eb535f3a4677fc4820158dca2b518e70
Betty Alagwu <bettyalagwu@gmail.com>
2022-12-09 07:28:24 +0800

    new commit

--- 0-main.py
+++ 0-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simpl
 
 matrix = [
     [1, 2, 3],
